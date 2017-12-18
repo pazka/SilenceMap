c90dc57d8ad0d6e6c84bda351f84c80c3d1d6de2
WEISSER Alexandre <alexandre.weisser@azothresearch.com>
2017-12-18 20:16:08 +0100

    db connection

--- persist/silence.js
+++ persist/silence.js
@@ -1,13 +1,13 @@
 var Sequelize = require('sequelize');
-/*var sequelize = new Sequelize(
-    'sound',
-     process.env.bdduser,
-      process.env.bddpsw,
-      {host: '54.37.70.57',
+var sequelize = new Sequelize(
+    'silence',
+     'root',
+      '',
+      {host: 'localhost',
       dialect: 'mysql',
       // disable logging; default: console.log
-      logging: false});*/
-var sequelize = new Sequelize('mysql://'+process.env.bdduser+":"+process.env.bddpsw+'@54.37.70.57:3306/Silence')
+      logging: false});
+//var sequelize = new Sequelize('mysql://'+process.env.bdduser+":"+process.env.bddpsw+'@54.37.70.57:3306/Silence')
 var sound = sequelize.define('sound', {
     id: {type: Sequelize.INTEGER, primaryKey : true},
     file: Sequelize.TEXT,
